801eaf07379819247a0cc0e2f3533771e2e5a387
Dkilpatrick31 <dkilpatrick1990@gmail.com>
2019-08-08 20:58:24 -0500

    Fixed the transform and A key.

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ function removeTransition(e) {
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-  // console.log(audio);
   if (!audio) return;
 
   key.classList.add('playing');
@@ -17,37 +16,3 @@ function playSound(e) {
 const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
-
-window.addEventListener('keydown', function(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  if(!audio) return; // stop the function from running all together
-  audio.currentTime = 0; // rewind to the start
-  audio.play();
-  key.classList.add('playing');
-});
-
-function removeTransition(e) {
-  if(e.propertName !== 'transform') return; // skip it if it's not a transform
-  console.log(this);
-};
-// 
-// const keys = document.querySelectorAll('.key');
-// keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-//
-// function myFunction() {
-//   document.getElementById('myDropdown').classList.toggle("show");
-// }
-
-// window.onclick = function(event) {
-//   if (!event.target.matches('.dropdownButton')) {
-//     let dropdowns = document.getElementsByClassName("dropdown-content");
-//     let i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       let openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//       }
-//     }
-//   }
-// }
